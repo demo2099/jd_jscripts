f11c4272847d4defb4451ff8d17b572236547fa0
demo <available3099@gmail.com>
2021-02-01 18:09:19 -0800

    debug

--- format_share_jd_code.js
+++ format_share_jd_code.js
@@ -1100,16 +1100,15 @@ function showFormatMsg() {
   console.log(`/jdzz ${jdzz.join('&')}\n`)
   console.log(`/jdnian ${jdnian.join('&')}\n`)
 
+
+
   console.log(`\n========== 【格式化互助码@】 ==========`)
-  console.log(`/submit_bean_code ${submit_bean_code.join('@')}\n`)
-  console.log(`/submit_farm_code ${submit_farm_code.join('@')}\n`)
-  console.log(`/submit_pet_code ${submit_pet_code.join('@')}\n`)
+  console.log(`jd_plantBean ${submit_bean_code.join('@')}\n`)
+  console.log(`jd_fruit ${submit_farm_code.join('@')}\n`)
+  console.log(`jd_pet ${submit_pet_code.join('@')}\n`)
   console.log(`/submit_jxfactory_code ${submit_jxfactory_code.join('@')}\n`)
   console.log(`/submit_ddfactory_code ${submit_ddfactory_code.join('@')}\n`)
-  console.log(
-    `/submit_zodiac_tempcode ${submit_zodiac_tempcode.join('@')}\n`
-  )
-  console.log(`\n提交机器人 @Commit Code Bot\n`)
+  console.log(`/submit_zodiac_tempcode ${submit_zodiac_tempcode.join('@')}\n`)
   console.log(`/jdcash ${jdcash.join('@')}\n`)
   console.log(`/jdcrazyjoy ${jdcrazyjoy.join('@')}\n`)
   console.log(`/jdnh ${jdnh.join('@')}\n`)
