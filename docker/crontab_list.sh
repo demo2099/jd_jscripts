b596f11df796a504842eb60184d6744615af1343
demo <available3099@gmail.com>
2021-02-20 04:09:27 -0800

    crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -12,6 +12,9 @@
 
 #女装盲盒 活动时间：2021-2-19至2021-2-25
 5 7,23 19-25 2 * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
+
+#京东极速版天天领红包 活动时间：2021-1-18至2021-3-3
+5 0,23 * * * node /scripts/js_redpacket.js >> /scripts/logs/js_redpacket.log 2>&1
 ##############长期活动##############
 # 签到
 3 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
@@ -97,6 +100,8 @@
 1 7,12,19 * * * node /scripts/jd_beauty.js >> /scripts/logs/jd_beauty.log 2>&1
 #京东保价
 1 0,23 * * * node /scripts/jd_price.js >> /scripts/logs/jd_price.log 2>&1
+#京东极速版签到+赚现金任务
+1 1,6 * * * node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
 # 源头好物红包
