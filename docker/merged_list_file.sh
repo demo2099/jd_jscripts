550bcc4778cc6f18d399a26324d3f45788550738
demo <available3099@gmail.com>
2021-03-14 18:09:05 -0700

    10000

--- docker/merged_list_file.sh
+++ docker/merged_list_file.sh
@@ -120,4 +120,4 @@
 23 5,23 * * * node /scripts/jd_jxd.js |ts >> /scripts/logs/jd_jxd.log 2>&1
 
 # 必须要的默认定时任务请勿删除
-8 9 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
+14 20 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
