68e7873fe7b7a28cae865388f70c6cc1b835c8ae
demo <available3099@gmail.com>
2021-04-23 00:09:00 -0700

    以-开头的pin无法被printf的bug

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -115,7 +115,11 @@
 #京小兑
 13 8,16,20 * * * node /scripts/jd_jxd.js >> /scripts/logs/jd_jxd.log 2>&1
 #京东健康社区
-30 0-23/3 * * * node /scripts/jd_health.js >> /scripts/logs/jd_health.log 2>&1
+13 1,22 * * * node /scripts/jd_health.js >> /scripts/logs/jd_health.log 2>&1
+#京东健康社区收集健康能量
+5-45/20 * * * * node /scripts/jd_health_collect.js >> /scripts/logs/jd_health_collect.log 2>&1
+# 幸运大转盘
+10 10,23 * * * node /scripts/jd_market_lottery.js >> /scripts/logs/jd_market_lottery.log 2>&1
 
 
 5 1,6,11,16,21 * 3-4 *   node /scripts/z_super5g.js  >> /scripts/logs/z_super5g.js 2>&1
