ed7c87b2aa3b00e8f2b632493581e867492493b7
demo <available3099@gmail.com>
2021-05-22 02:08:58 -0700

    修复BUG

--- docker/merged_list_file.sh
+++ docker/merged_list_file.sh
@@ -121,4 +121,4 @@
 15 0,12,22 * * * node /scripts/jd_jump.js |ts >> /scripts/logs/jd_jump.log 2>&1
 
 # 必须要的默认定时任务请勿删除
-6 17 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
+38 1 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
