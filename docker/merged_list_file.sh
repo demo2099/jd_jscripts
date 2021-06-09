930e95e31fba27f0dc6032179e693cc9c6f51b89
demo <available3099@gmail.com>
2021-06-08 23:08:57 -0700

    优化

--- docker/merged_list_file.sh
+++ docker/merged_list_file.sh
@@ -143,4 +143,4 @@
 15 0,12,22 * * * node /scripts/jd_jxmc.js |ts >> /scripts/logs/jd_jxmc.log 2>&1
 
 # 必须要的默认定时任务请勿删除
-24 13 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
+28 4 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
